bcc6dbbe9574a4b222eb3227aa16c048b75b2fc9
Zenia Gist <zeniagist@gmail.com>
2020-12-17 10:58:50 +0000

    added code structure through comments to updated databases

---
+++ mynotes.js
@@ -0,0 +1,14 @@
+$(function(){
+  // define variables
+  // load notes on page load: AJAX Call to loadnotes.php
+  // add a new note:  AJAX Call to createnote.php
+  // type note:  AJAX Call to updatenote.php
+  // click on all notes button
+  // click on done after editing: load notes again
+  // click on edit: go to edit mode show: delete buttons, ...
+
+  // functions
+    // click on a note
+    // click on delete
+    // show hide function
+});
---
+++ updatenote.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+include('connection.php');
+
+// get the id of note sent through AJAX
+
+// get the content of the note
+
+// get time
+
+// run a query to update the note
+
+?>
