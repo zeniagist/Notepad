24d2942e5f7863bb1871216992907c25a07f364d
Zenia Gist <zeniagist@gmail.com>
2020-12-17 12:47:12 +0000

    showed editing buttons and hid the other buttons when clicking Add Note button

--- mynotes.js
+++ mynotes.js
@@ -1,5 +1,7 @@
 $(function(){
   // define variables
+  var activeNote = 0;
+  
   // load notes on page load: AJAX Call to loadnotes.php
   $.ajax({
     url: "notes/loadnotes.php",
@@ -9,6 +11,36 @@ $(function(){
   });
 
   // add a new note:  AJAX Call to createnote.php
+  $('#addNote').click(function(){
+      $.ajax({
+          url: "notes/createnote.php",
+          success: function(data){
+              if(data == 'error'){
+                  $('#alertContent').text("There was an issue inserting the new note in the database!");
+                  $('#alert').fadeIn();
+              }else{
+                // update activeNote to the id of the new note
+                activeNote = data;
+                $("textarea").val("");
+                
+                // show hide elements
+                //showHide(["#notepad", "#allNotes", "#done"], ["#notes", "#addNote", "#edit"]);
+                // show elements
+                $("#notepad").show();
+                $("#allNotes").show();
+                $("#done").show();
+                
+                // hide elements
+                $("#notes").hide();
+                $("#addNote").hide();
+                $("#edit").hide();
+                
+                $("textarea").focus();
+              }
+          }
+      });
+  });
+  
   // type note:  AJAX Call to updatenote.php
   // click on all notes button
   // click on done after editing: load notes again
@@ -18,4 +50,14 @@ $(function(){
     // click on a note
     // click on delete
     // show hide function
+    // function showHide(array1, array2){
+    //     for(i=0; i<array.length; i++){
+    //       $(array1[i]).show();
+    //     }
+        
+    //     for(i=0; i<array.length; i++){
+    //       $(array2[i]).hide();
+    //     }
+    // }
+    
 });
