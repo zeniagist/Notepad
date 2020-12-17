28d288fe318d5e04b13de252d876f5b81685f1ad
Zenia Gist <zeniagist@gmail.com>
2020-12-17 11:57:51 +0000

    delete blank notes and added test notes from table

--- loadnotes.php
+++ loadnotes.php
@@ -9,19 +9,32 @@ $user_id = $_SESSION['user_id'];
 $sql = "DELETE FROM notes WHERE note=''";
 $result = mysqli_query($link, $sql);
 
+// show notes or alert message
+if(!$result){
+    echo '<div class="alert alert-warning">There was an error deleting notes!</div>';
+    exit;
+}
+
 // run a query to look for notes corresponding to user_id
+$sql ="SELECT * FROM notes WHERE user_id='$user_id' ORDER BY time DESC";
 
 // show notes or alert message
-
-echo "<div class='noteheader'>
-        <div class='text'>This is a note This is a note This is a note This is a note This is a note 
-            This is a note 
-        </div>
-        <div class='timetext'>May 30, 2016 07:00:00 PM</div>
-      </div>";
-
-echo "<div class='noteheader'>
-        <div class='text'>This is a note</div>
-        <div class='timetext'>May 30, 2016 07:00:00 PM</div>
-      </div>";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result)>0){
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            $note = $row['note'];
+            $time = $row['time'];
+            
+           echo "<div class='noteheader'>
+                    <div class='text'>$note</div>
+                    <div class='timetext'>$time</div>
+                  </div>";
+        }
+    }else{
+        echo '<div class="alert alert-warning">You have not created any notes yet</div>';
+    }
+}else{
+    echo '<div class="alert alert-warning">There was an error deleting notes!</div>';
+    //echo "ERROR: Unable to execute: $sql "mysqli_error($link);
+}
 ?>
