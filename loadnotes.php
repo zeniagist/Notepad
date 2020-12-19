4cb2c4dbf6b21bf71fc51a5af17d202bde1a618a
Zenia Gist <zeniagist@gmail.com>
2020-12-19 22:03:12 +0000

    added 100% width to delete button to ensure correct size

--- loadnotes.php
+++ loadnotes.php
@@ -30,7 +30,7 @@ if($result = mysqli_query($link, $sql)){
            echo "
                 <div class='note'>
                     <div class='col-xs-5 col-sm-3 delete'>
-                        <button class='btn btn-lg btn-danger'>delete</button>
+                        <button class='btn btn-lg btn-danger' style='width:100%'>delete</button>
                     </div>
                     
                     <div class='noteheader' id='$note_id'>
